58ce105b95f5443db25e66d675c95213cd2cd1fc
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-05-29 22:44:18 +0200

    Adding dependencies to install script

--- install-all.sh
+++ install-all.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
+
+if [ "$1" == "-r" ]; then
+	remflag=$1
+else
+	name=$1
+	remflag=$2
+fi
+
+if test -z "$name"
+then
+	name="master-env"
+fi
+
+ENVS=$(conda env list | awk '{print $1 }')
+if [[ $ENVS = *"$name"* ]]; then
+	if [ "$remflag" == "-r" ]; then
+		conda deactivate &&
+		conda env remove --name "$name"
+		printf "Environment '$name' removed\n\n"
+		printf "Installing '$name' environment\n\n"
+	else
+		printf "Updating '$name' environment\n\n"
+	fi
+else
+	printf "Environment '$name' not exists\n\n"
+	printf "Installing '$name' environment\n\n"
+fi
+
+conda deactivate &&
 conda create -n master-env python=3.7 -y &&
 conda activate master-env &&
+conda install -y -c conda-forge jupyterlab=3.0.16 &&
 conda install -y -c anaconda numpy=1.19.2 &&
-conda install -y -c anaconda pandas=1.2.4
+conda install -y -c anaconda pandas=1.2.4 &&
+conda install -y -c anaconda openpyxl=3.0.7 &&
+conda install -y -c conda-forge matplotlib=3.4.2 &&
+conda install -y -c anaconda seaborn=0.11.1
+
+printf "\nEvnironment '$name' activated\n"
